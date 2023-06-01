3d66dff4d0c6726d062785993d684d78f61b2816
Filip Michálek <vsechnouzjezabrany@gmail.com>
2023-06-02 01:28:40 +0200

    3nd

--- js/eveningSliderScript.js
+++ js/eveningSliderScript.js
@@ -5,107 +5,92 @@ const eveningSlides = document.querySelectorAll(".evening_slider_box__slide");
 const eveningPrevButton = document.getElementById("evening_prev_button");
 const eveningNextButton = document.getElementById("evening_next_button");
 
-
-eveningIndexDisplay = document.getElementById("evening_current");
-let eveningCurrentIndex = 0;
-eveningIndexDisplay.innerHTML = eveningCurrentIndex + 1;
+let eveningCurrentIndex = parseInt(localStorage.getItem('eveningCurrentIndex')) || 0;
+eveningSliderBox.scrollLeft = eveningSlides[eveningCurrentIndex].offsetLeft;
+document.getElementById("evening_current").innerHTML = (eveningCurrentIndex + 1);
 
 const eveningAmount = eveningSliderBox.children.length;
-
 document.getElementById("evening_amount").innerHTML = eveningAmount;
 
-const eveningSliderPrevButton = document.getElementById("evening_prev_button")
-const eveningSliderNextButton = document.getElementById("evening_next_button")
+const eveningSliderPrevButton = document.getElementById("evening_prev_button");
+const eveningSliderNextButton = document.getElementById("evening_next_button");
 
 eveningPrevButton.addEventListener('click', () => {
-if (eveningCurrentIndex > 0) {
+  if (eveningCurrentIndex > 0) {
     eveningCurrentIndex--;
     eveningSliderBox.scrollLeft = eveningSlides[eveningCurrentIndex].offsetLeft;
     setTimeout(() => {
-      eveningIndexDisplay.innerHTML = eveningCurrentIndex + 1;
-      }, 500); // Wait 500 milliseconds for the animation to finish
-}
-// prev_button
-if (eveningCurrentIndex == 0) {
-  eveningSliderPrevButton.style.visibility = 'hidden';
-  eveningSliderPrevButton.style.opacity = '0';
-} else {
-  eveningSliderPrevButton.style.visibility = 'visible';
-  eveningSliderPrevButton.style.opacity = '1';
-}
+      document.getElementById("evening_current").innerHTML = (eveningCurrentIndex + 1);
+    }, 500); // Wait 500 milliseconds for the animation to finish
+  }
 
-// next_button
-if (eveningCurrentIndex == eveningSlides.length - 1) {
-  eveningSliderNextButton.style.visibility = 'hidden';
-  eveningSliderNextButton.style.opacity = '0';
-} else {
-  eveningSliderNextButton.style.visibility = 'visible';
-  eveningSliderNextButton.style.opacity = '1';
-}
+  if (eveningCurrentIndex == 0) {
+    eveningSliderPrevButton.style.visibility = 'hidden';
+    eveningSliderPrevButton.style.opacity = '0';
+  } else {
+    eveningSliderPrevButton.style.visibility = 'visible';
+    eveningSliderPrevButton.style.opacity = '1';
+  }
 
+  if (eveningCurrentIndex == eveningSlides.length - 1) {
+    eveningSliderNextButton.style.visibility = 'hidden';
+    eveningSliderNextButton.style.opacity = '0';
+  } else {
+    eveningSliderNextButton.style.visibility = 'visible';
+    eveningSliderNextButton.style.opacity = '1';
+  }
 });
 
 eveningNextButton.addEventListener('click', () => {
-if (eveningCurrentIndex < eveningSlides.length - 1) {
+  if (eveningCurrentIndex < eveningSlides.length - 1) {
     eveningCurrentIndex++;
     eveningSliderBox.scrollLeft = eveningSlides[eveningCurrentIndex].offsetLeft;
     setTimeout(() => {
-      eveningIndexDisplay.innerHTML = eveningCurrentIndex + 1;
-      }, 500); // Wait 500 milliseconds for the animation to finish
-    
-}
-if (eveningCurrentIndex == 0) {
-  eveningSliderPrevButton.style.visibility = 'hidden';
-  eveningSliderPrevButton.style.opacity = '0';
-} else {
-  eveningSliderPrevButton.style.visibility = 'visible';
-  eveningSliderPrevButton.style.opacity = '1';
-}
+      document.getElementById("evening_current").innerHTML = (eveningCurrentIndex + 1);
+    }, 500); // Wait 500 milliseconds for the animation to finish
+  }
 
-// next_button
-if (eveningCurrentIndex == eveningSlides.length - 1) {
-  eveningSliderNextButton.style.visibility = 'hidden';
-  eveningSliderNextButton.style.opacity = '0';
-} else {
-  eveningSliderNextButton.style.visibility = 'visible';
-  eveningSliderNextButton.style.opacity = '1';
-}
+  if (eveningCurrentIndex == 0) {
+    eveningSliderPrevButton.style.visibility = 'hidden';
+    eveningSliderPrevButton.style.opacity = '0';
+  } else {
+    eveningSliderPrevButton.style.visibility = 'visible';
+    eveningSliderPrevButton.style.opacity = '1';
+  }
+
+  if (eveningCurrentIndex == eveningSlides.length - 1) {
+    eveningSliderNextButton.style.visibility = 'hidden';
+    eveningSliderNextButton.style.opacity = '0';
+  } else {
+    eveningSliderNextButton.style.visibility = 'visible';
+    eveningSliderNextButton.style.opacity = '1';
+  }
 });
 
-// This checks the value of the eveningCurrentIndex, if its 0, previous button is hidden
-
-
 eveningSliderBox.addEventListener('scroll', () => {
-    eveningCurrentIndex = Math.round(eveningSliderBox.scrollLeft / eveningSliderBox.offsetWidth);
-    eveningIndexDisplay.innerHTML = eveningCurrentIndex + 1;
-    
-    if (eveningCurrentIndex == 0) {
-      eveningSliderPrevButton.style.visibility = 'hidden';
-      eveningSliderPrevButton.style.opacity = '0';
-    } else {
-      eveningSliderPrevButton.style.visibility = 'visible';
-      eveningSliderPrevButton.style.opacity = '1';
-    }
-    
-    // next_button
-    if (eveningCurrentIndex == eveningSlides.length - 1) {
-      eveningSliderNextButton.style.visibility = 'hidden';
-      eveningSliderNextButton.style.opacity = '0';
-    } else {
-      eveningSliderNextButton.style.visibility = 'visible';
-      eveningSliderNextButton.style.opacity = '1';
-    }
-  });
+  eveningCurrentIndex = Math.round(eveningSliderBox.scrollLeft / eveningSliderBox.offsetWidth);
+  document.getElementById("evening_current").innerHTML = (eveningCurrentIndex + 1);
 
-// Resizing the slider
+  if (eveningCurrentIndex == 0) {
+    eveningSliderPrevButton.style.visibility = 'hidden';
+    eveningSliderPrevButton.style.opacity = '0';
+  } else {
+    eveningSliderPrevButton.style.visibility = 'visible';
+    eveningSliderPrevButton.style.opacity = '1';
+  }
+
+  if (eveningCurrentIndex == eveningSlides.length - 1) {
+    eveningSliderNextButton.style.visibility = 'hidden';
+    eveningSliderNextButton.style.opacity = '0';
+  } else {
+    eveningSliderNextButton.style.visibility = 'visible';
+    eveningSliderNextButton.style.opacity = '1';
+  }
+
+  localStorage.setItem('eveningCurrentIndex', eveningCurrentIndex.toString());
+});
 
 window.addEventListener('resize', function() {
   const slider = document.getElementById('evening_slider_box');
   slider.click();
 });
-
-
-
-
-
-
--- js/morningSliderScript.js
+++ js/morningSliderScript.js
@@ -1,59 +1,96 @@
 // Morning Routine
 
 const morningSliderBox = document.getElementById("morning_slider_box");
-const morningSlides = document.querySelectorAll('.morning_slider_box__slide');
+const morningSlides = document.querySelectorAll(".morning_slider_box__slide");
 const morningPrevButton = document.getElementById("morning_prev_button");
 const morningNextButton = document.getElementById("morning_next_button");
 
-
-morningIndexDisplay = document.getElementById("morning_current");
-let morningCurrentIndex = 0;
-morningIndexDisplay.innerHTML = morningCurrentIndex + 1;
+let morningCurrentIndex = localStorage.getItem('morningCurrentIndex') || 0;
+morningSliderBox.scrollLeft = morningSlides[morningCurrentIndex].offsetLeft;
+document.getElementById("morning_current").innerHTML = parseInt(morningCurrentIndex) + 1;
 
 const morningAmount = morningSliderBox.children.length;
-
 document.getElementById("morning_amount").innerHTML = morningAmount;
 
+const morningSliderPrevButton = document.getElementById("morning_prev_button");
+const morningSliderNextButton = document.getElementById("morning_next_button");
+
 morningPrevButton.addEventListener('click', () => {
-if (morningCurrentIndex > 0) {
+  if (morningCurrentIndex > 0) {
     morningCurrentIndex--;
     morningSliderBox.scrollLeft = morningSlides[morningCurrentIndex].offsetLeft;
     setTimeout(() => {
-        morningIndexDisplay.innerHTML = morningCurrentIndex + 1;
-      }, 500); // Wait 500 milliseconds for the animation to finish
+      document.getElementById("morning_current").innerHTML = parseInt(morningCurrentIndex) + 1;
+    }, 500); // Wait 500 milliseconds for the animation to finish
+  }
 
-}
+  if (morningCurrentIndex == 0) {
+    morningSliderPrevButton.style.visibility = 'hidden';
+    morningSliderPrevButton.style.opacity = '0';
+  } else {
+    morningSliderPrevButton.style.visibility = 'visible';
+    morningSliderPrevButton.style.opacity = '1';
+  }
+
+  if (morningCurrentIndex == morningSlides.length - 1) {
+    morningSliderNextButton.style.visibility = 'hidden';
+    morningSliderNextButton.style.opacity = '0';
+  } else {
+    morningSliderNextButton.style.visibility = 'visible';
+    morningSliderNextButton.style.opacity = '1';
+  }
 });
 
 morningNextButton.addEventListener('click', () => {
-if (morningCurrentIndex < morningSlides.length - 1) {
+  if (morningCurrentIndex < morningSlides.length - 1) {
     morningCurrentIndex++;
     morningSliderBox.scrollLeft = morningSlides[morningCurrentIndex].offsetLeft;
     setTimeout(() => {
-        morningIndexDisplay.innerHTML = morningCurrentIndex + 1;
-      }, 500); // Wait 500 milliseconds for the animation to finish
-    
-}
+      document.getElementById("morning_current").innerHTML = parseInt(morningCurrentIndex) + 1;
+    }, 500); // Wait 500 milliseconds for the animation to finish
+  }
+
+  if (morningCurrentIndex == 0) {
+    morningSliderPrevButton.style.visibility = 'hidden';
+    morningSliderPrevButton.style.opacity = '0';
+  } else {
+    morningSliderPrevButton.style.visibility = 'visible';
+    morningSliderPrevButton.style.opacity = '1';
+  }
+
+  if (morningCurrentIndex == morningSlides.length - 1) {
+    morningSliderNextButton.style.visibility = 'hidden';
+    morningSliderNextButton.style.opacity = '0';
+  } else {
+    morningSliderNextButton.style.visibility = 'visible';
+    morningSliderNextButton.style.opacity = '1';
+  }
 });
 
 morningSliderBox.addEventListener('scroll', () => {
-    morningCurrentIndex = Math.round(morningSliderBox.scrollLeft / morningSliderBox.offsetWidth);
-    morningIndexDisplay.innerHTML = morningCurrentIndex + 1;
-  });
+  morningCurrentIndex = Math.round(morningSliderBox.scrollLeft / morningSliderBox.offsetWidth);
+  document.getElementById("morning_current").innerHTML = parseInt(morningCurrentIndex) + 1;
 
+  if (morningCurrentIndex == 0) {
+    morningSliderPrevButton.style.visibility = 'hidden';
+    morningSliderPrevButton.style.opacity = '0';
+  } else {
+    morningSliderPrevButton.style.visibility = 'visible';
+    morningSliderPrevButton.style.opacity = '1';
+  }
 
-// Resizing the slider
+  if (morningCurrentIndex == morningSlides.length - 1) {
+    morningSliderNextButton.style.visibility = 'hidden';
+    morningSliderNextButton.style.opacity = '0';
+  } else {
+    morningSliderNextButton.style.visibility = 'visible';
+    morningSliderNextButton.style.opacity = '1';
+  }
+
+  localStorage.setItem('morningCurrentIndex', morningCurrentIndex);
+});
 
 window.addEventListener('resize', function() {
   const slider = document.getElementById('morning_slider_box');
   slider.click();
 });
-
-
-
-
-
-
-
-
-
